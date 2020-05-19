7e3e4636015a8470cf5f7118d6801bf60f9f017b
TiagoHucs <tiagohucs@gmail.com>
2020-05-19 17:34:19 -0300

    arquitetura inicial

--- src/main/java/com/hucs/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/hucs/helpdesk/HelpdeskApplication.java
@@ -2,6 +2,7 @@ package com.hucs.helpdesk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class HelpdeskApplication {
@@ -9,5 +10,11 @@ public class HelpdeskApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
 	}
+
+	@RequestMapping("/")
+	String index() {
+		return "index";
+	}
+
 
 }
